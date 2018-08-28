9d1e9b80fbbcc1ff1305163e75d8d6aa11bb4497
Marek Sautter <marek.sautter@gmail.com>
2018-08-28 14:42:22 -0700

    Started object oriented programming
    -Created an object for playlist

--- main.py
+++ main.py
@@ -6,65 +6,7 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 from os import system, name
-
-# print(json.dumps(searchResults, sort_keys=True, indent=4))
-# spotify:user:1251475457
-
-def printPlaylists(playlists) :
-    # clear()
-    index = 0
-    for item in playlists:
-        index+=1
-        print(">>> PLAYLIST: " + str(index))
-        print("       PNAME:        " + item["name"])
-        print("         PID:        " + item["uri"])
-        print("       COUNT:        " + str(item["tracks"]["total"]))
-    input("Press any key to continue...")
-    
-def playlistMenu(playlists, searchTerm, tPlaylists, tTracks):
-    while True:
-        #clear()
-        print()
-        print(">>> Playlist Menu <<<")
-        print("       KEYWORD: " + searchTerm)
-        print("PLAYLIST COUNT: " + str(tPlaylists))
-        print("   TRACK COUNT: " + str(tTracks))
-        print("What would you like to do with these playlists?")
-        print("4 - Run Data Analysis on these playlists")
-        print("3 - Print Playlists to file")
-        print("2 - Print Playlists to console")
-        print("1 - Exit")
-        choice = input("\t\tYour choice: ")
-        if choice == "4":
-            playlistDataAnalysis(playlists, tPlaylists, tTracks)
-        if choice == "3":
-            print("COMING SOON")
-        if choice == "2":
-            printPlaylists(playlists)
-        if choice == "1":
-            exitProgram()
-
-def playlistDataAnalysis(playlists, tPlaylists, tTracks):
-    playlistIndex = 0
-    trackIndex = 0
-    for item in playlists:
-        playlistIndex +=1
-        # print("Playlist: " + item['name'])
-        # print(str(playlistIndex) + "/" + str(tPlaylists) + "playlists")
-        trackRequest = spotifyObject.user_playlist_tracks(item['owner']['id'], playlist_id=item['id'], fields=None, limit=100, offset=0, market=None)
-        tracks = trackRequest['items']
-        for track in tracks:
-            clear()
-            trackIndex+=1
-            print(str(trackIndex) + "/" + str(tTracks) + " tracks")
-            # print(track['track']['name'])
-            f = open("demofile.txt", "a")
-            try: 
-                print((str(track["track"]["name"]) + ", " + str(track["track"]["album"]["artists"][0]["name"])).encode('utf8'))
-            except:
-                print("Shit's fucked yo")
-            # f.write("\n" + str(track["track"]["name"]) + "," + str(track["track"]["album"]["artists"][0]["name"]))
-        #print(json.dumps(tracks, sort_keys=True, indent=4))
+import csv
 
 def clear():
     if name == 'nt':
@@ -76,7 +18,17 @@ def exitProgram() :
     print("Thank you for using Spotipy Playlist Scanner")
     exit()
 
-# ////////START OF PROGRAM//////////
+
+
+fileName = ""
+listOfPlaylists = []
+
+class Playlist(object):
+    def __init__(self, name=None, URI=None, uURI=None, total=None):
+        self.name = name
+        self.URI = URI
+        self.uURI = uURI
+        self.total = total
 
 
 # Get username from terminal
@@ -98,33 +50,53 @@ user = spotifyObject.current_user()
 displayName = user['display_name']
 followers = user['followers']['total']
 
-while True:
-    #clear()
-    print("Welcome! " + displayName)
-    print("Followers: " + str(followers))
-    print()
-    print(">>> MAIN MENU <<<")
-    print()
-    print("2 - Playlist Scanner")
-    print("1 - Exit")
-    choice = input("\t\tYour choice: ")
-    # Search for playlist
-    if choice == "2":
-        searchQuery = input("\t\tKeyword: ")
-        print()
-        searchResults = spotifyObject.search(searchQuery,50,0,"playlist")
-        if searchResults['playlists']['total'] == 0:
-            print("Sorry, that value did not return any playlists. Please try again")
-        else:
-            searchResults2 = spotifyObject.search(searchQuery,50,50,"playlist")
-            playlists = searchResults['playlists']['items']
-            playlists += searchResults2['playlists']['items']
-            index = 0
-            total = 0
-            for item in playlists:
-                index+=1
-                total += item["tracks"]["total"]
-            playlistMenu(playlists, searchQuery, index, total)
-    if choice == "1":
-        exitProgram()
+clear()
+print("Welcome! " + displayName)
+print("Followers: " + str(followers))
+# Search for playlist
+searchQuery = input("Search Word: ")
+print()
+searchResults = spotifyObject.search(searchQuery,50,0,"playlist")
+if searchResults['playlists']['total'] == 0:
+    print("Sorry, that value did not return any playlists.")
+    input()
+    exitProgram()
+searchResults2 = spotifyObject.search(searchQuery,50,50,"playlist")
+playlists = searchResults['playlists']['items']
+playlists += searchResults2['playlists']['items']
+totalPlaylists = 0
+totalTracks = 0
+for item in playlists:
+    totalPlaylists+=1
+    totalTracks += item["tracks"]["total"]
 
+print("Your search term, \'{}\', returned {} tracks across {} playlists".format(searchQuery, totalTracks, totalPlaylists))
+
+choice = input("Would you like to print playlists? (y/n)")
+if choice == 'y':
+    index = 0
+    for item in playlists :
+        index+=1
+        print(">>> PLAYLIST: " + str(index))
+        print("       PNAME:        " + item["name"])
+        print("         PID:        " + item["uri"])
+        print("       COUNT:        " + str(item["tracks"]["total"]))
+        listOfPlaylists.append(Playlist(item['name'],item['uri'],item['owner']['id'], item['tracks']['total']))
+    input("Press any key to continue...")
+    clear()
+
+print("   Search Term: " + searchQuery)
+print("   Track Count: " + str(totalTracks))
+print("Playlist Count: " + str(totalPlaylists))
+
+
+choice = input("Would you like to print tracks to a file? (y/n)")
+if choice == 'y':
+    fileName = input("What would you like to name the file (.csv)? ")
+    with open(fileName, 'w', newline=' ') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        testSearchResults = spotifyObject.search(searchQuery,50,0,"playlist")
+        for playlist in testSearchResults['items']:
+            print (str(playlist['name']))
+            print ('total tracks: ' + str(playlist['tracks']['total']))
+            # results = spotifyObject.user_playlist()
